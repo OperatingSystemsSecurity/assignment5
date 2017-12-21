f0008264035dc1d955c0da035c2035396e7bb44b
Hendrikto <hendrik.to@gmail.com>
2017-12-22 00:06:43 +0100

    Simplify code

--- covert_channel/covert_receiver.sh
+++ covert_channel/covert_receiver.sh
@@ -9,8 +9,7 @@ usage() {
 (( $1 < 0 )) && echo 'Threshold must be >= 0' && usage && exit 2
 
 while true; do
-	task_time=$((time ./cpu_intensive_task.sh) 2>&1 | grep -oP '^real\s+[\d.]+m\K[\d.]+s$')
-	task_time_milliseconds=$(($(grep -oP '\d+(?=\.)' <<< $task_time) * 1000 + $(grep -oP '\.0*\K\d+' <<< $task_time)))
-	[[ $DEBUG ]] && echo "Task time: $task_time_milliseconds"
-	echo Received: $(($task_time_milliseconds > $1))
+	task_time=$((time ./cpu_intensive_task.sh) 2>&1 | grep -oP '^real\s+[\d.]+m\K[\d.]+(?=s$)' | tr -d . | sed -r s/^0+//)
+	[[ $DEBUG ]] && echo "Task time: $task_time"
+	echo Received: $(($task_time > $1))
 done
